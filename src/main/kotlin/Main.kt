dbe657ff62d1c090b038d5f9c6e8829e9b4972a5
Husoid <husoid@mail.ru>
2023-06-15 11:41:44 +0500

    work error

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 fun main(args: Array<String>) {
-    val like = 1015
-    val variant = if (like % 10 == 1 && like != 11) {
+    val like = 21
+    val variant = if (like % 10 == 1 && like % 100 != 11) {
         "человеку"
     } else {
         "людям"
     }
+    println(512 % 10)
     println("Понравилось $like $variant")
 }
